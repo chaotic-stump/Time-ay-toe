fe05e70a85ae21f92a955d4466e11ca69af6efed
Sean Parker <seanrichardparker@gmail.com>
2025-04-16 23:52:21 -0400

    add initial timer state + render

--- timer.js
+++ timer.js
@@ -2,4 +2,7 @@ let title = document.getElementById("timer-title");
 let timer = document.getElementById("timer");
 
 title.innerText = "hooked up!"
-timer.innerText = "30:00"
+// timer.innerText = "30:00"
+
+let pomoDoro = 25;
+timer.innerText = pomoDoro + ":00"
