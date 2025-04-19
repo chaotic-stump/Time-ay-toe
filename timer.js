898978a5ebda218520c836086b2bd20d79b480e5
Sean Parker <seanrichardparker@gmail.com>
2025-04-19 16:48:12 -0400

    change some controls to a select box, get progress bar, timer, and start button working

--- timer.js
+++ timer.js
@@ -1,16 +1,78 @@
-let title = document.queryBySelector("#timer-title");
-let timer = document.queryBySelector("#timer");
+let title = document.querySelector("#timer-title");
+let timer = document.querySelector("#timer");
+let seletedStage = document.querySelector("#selected-stage");
+let timerOptions = document.querySelector("#timer-options");
+let startButton = document.querySelector("#start-button");
+let progressBar = document.querySelector(".bar");
+
+let durations = {
+  pomodoro: 25,
+  shortBreak: 1,
+  longBreak: 10
+};
+
+let countdownInterval;
 
 
-function buildStartTimer(stage) {
-  title.innerText = stage + ":00"
+let updateTimerDisplay = (minutes, seconds) => {
+  timer.innerText = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 }
 
-// title.innerText = buildStartTimer(shortBreak);
+let updateTimerFromDropdown = () => {
+  seletedStage.innerText = timerOptions.options[timerOptions.selectedIndex].text;
+  let selectedTime = timerOptions.value;
+  let minutes = durations[selectedTime];
+  let seconds = 0;
 
-buildStartTimer(shortBreak);
-// timer.innerText = "30:00"
+  updateTimerDisplay(minutes, seconds);
+};
 
-let pomoDoro = 25;
-let shortBreak = 5;
-let longBreak = 10;
+updateTimerFromDropdown();
+
+let startTimer = () => {
+  clearInterval(countdownInterval);
+
+  let selectStage = timerOptions.value;
+  let minutes = durations[selectStage];
+  let seconds = 0;
+  let elapsedTime = 0;
+  let totalTime = minutes * 60 + seconds;
+
+  updateTimerDisplay(minutes, seconds);
+
+  countdownInterval = setInterval(() => {
+    elapsedTime++;
+    console.log(elapsedTime);
+    let progress = (elapsedTime / totalTime) * 100;
+    progressBar.style.width = `${progress}%`;
+
+    if (seconds === 0) {
+      if (minutes === 0) {
+        clearInterval(countdownInterval);
+        progressBar.style.width = `100%`;
+        progressBar.style.borderRadius = `3px`;
+        alert("Time's up!");
+        return;
+      }
+      minutes--;
+      seconds = 59;
+    } else {
+      seconds--;
+    }
+
+    updateTimerDisplay(minutes, seconds);
+  }, 1000);
+};
+
+let pauseTimer = () => {
+  clearInterval(countdownInterval);
+};
+
+let resetTimer = () => {
+  clearInterval(countdownInterval);
+  timer.innerText = "00:00";
+};
+
+
+
+timerOptions.addEventListener("change", updateTimerFromDropdown);
